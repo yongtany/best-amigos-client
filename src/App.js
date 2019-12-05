a3dc9c1490eb7c0bb93d2b1358ffd3f3bd864bfd
yongtany <yongn1494@gmail.com>
2019-12-05 20:09:34 +0900

    [CREATE] Setup Material-UI and add a Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Our app</h1>
-    </div>
-  );
+// Components
+import Navbar from './components/Navbar';
+
+// Page
+import home from './pages/HomePage';
+import login from './pages/LoginPage';
+import signup from './pages/SignupPage';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">  
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route path="/login" component={login} />
+              <Route path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
