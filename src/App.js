d2620c8cec210e59ed1c2eae303e506713293f2e
yongtany <yongn1494@gmail.com>
2019-12-05 22:37:49 +0900

    [CREATE] Add MuiTheme and Scream Card Details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
 import Navbar from './components/Navbar';
@@ -11,21 +13,43 @@ import login from './pages/LoginPage';
 import signup from './pages/SignupPage';
 
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">  
-            <Switch>
-              <Route exact path="/" component={home} />
-              <Route path="/login" component={login} />
-              <Route path="/signup" component={signup} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+     <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">  
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route path="/login" component={login} />
+                <Route path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+     </MuiThemeProvider>
     );
   }
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid'
+
+import Scream from '../components/Scream';
+
 
 class HomePage extends Component {
+  state = {
+    screams: null
+  }
+  componentDidMount() {
+    axios.get('/screams')
+      .then(res => {
+        this.setState({
+          screams: res.data
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream scream={scream} key={scream.screamId}/>)
+    ) : <p>Loading...</p>
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
     )
   }
 }
