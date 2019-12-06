92d71b2dd219cdb99aed3d18c8fa59826f4a9f40
yongtany <yongn1494@gmail.com>
2019-12-06 15:59:17 +0900

    [CREATE] Login and Signup Form

--- src/App.js
+++ src/App.js
@@ -3,35 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme';
+import jwtDecoode from 'jwt-decode';
 
 // Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 
 // Page
 import home from './pages/HomePage';
 import login from './pages/LoginPage';
 import signup from './pages/SignupPage';
 
+const theme = createMuiTheme(themeFile);
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
-  },
-  typography: {
-    useNextVariants: true
+let authenticated;
+const token = localStorage.FBIdToken;
+if(token) {
+  const decodedToken = jwtDecoode(token);
+  console.log(decodedToken);
+  if(decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login';
+    authenticated = false;
+  } else {
+    authenticated = true
   }
-});
+}
 
 class App extends Component {
   render() {
@@ -43,8 +40,16 @@ class App extends Component {
             <div className="container">  
               <Switch>
                 <Route exact path="/" component={home} />
-                <Route path="/login" component={login} />
-                <Route path="/signup" component={signup} />
+                <AuthRoute 
+                  path="/login" 
+                  component={login} 
+                  authenticated={authenticated} 
+                />
+                <AuthRoute 
+                  path="/signup" 
+                  component={signup} 
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
           </Router>
--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI Stuff
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import dayjs from 'dayjs';
 
 const styles = {
   card: {
@@ -24,6 +26,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const { classes, scream: { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount } } = this.props;
     return (
       <div>
@@ -42,7 +45,9 @@ class Scream extends Component {
               >
                 {userHandle}
               </Typography>
-              <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+              <Typography variant="body2" color="textSecondary">
+                {dayjs(createdAt).fromNow()}
+              </Typography>
               <Typography variant="body1">{body}</Typography>
             </CardContent>
         </Card>
