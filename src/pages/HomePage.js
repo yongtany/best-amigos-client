57fc934c8ba29a4b78df5c0abb4f45e749d249c4
yongtany <yongn1494@gmail.com>
2019-12-07 16:54:52 +0900

    [CREATE] Like and Unlike Actions

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import PropTypes from 'prop-types';
+
+// MUI stuff
 import Grid from '@material-ui/core/Grid'
 
 import Scream from '../components/Scream';
 import Profile from '../components/Profile';
 
+// Redux stuff
+import { connect } from 'react-redux';
+import { getScreams } from '../redux/actions/dataActions';
+
 class HomePage extends Component {
-  state = {
-    screams: null
-  }
+  
   componentDidMount() {
-    axios.get('/screams')
-      .then(res => {
-        this.setState({
-          screams: res.data
-        })
-      })
-      .catch(err => console.log(err));
+    this.props.getScreams();
   }
 
   render() {
-    let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map(scream => <Scream scream={scream} key={scream.screamId}/>)
+    const { screams, loading } = this.props.data;
+    let recentScreamsMarkup = !loading ? (
+      screams.map(scream => <Scream scream={scream} key={scream.screamId}/>)
     ) : <p>Loading...</p>
     return (
       <Grid container spacing={16}>
@@ -36,4 +36,13 @@ class HomePage extends Component {
   }
 }
 
-export default HomePage
+HomePage.propTypes = {
+  getScreams: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+export default connect(mapStateToProps, { getScreams })(HomePage)
