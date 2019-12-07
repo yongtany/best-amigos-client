753da0ad1b49c02801bba92e6c888034b39b7894
yongtany <yongn1494@gmail.com>
2019-12-07 17:48:38 +0900

    [CREATE] Delete Button

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,4 +1,4 @@
-import { SET_SCREAMS, LIKE_SCREAM, UNILIKE_SCREAM, LOADING_DATA, UNLIKE_SCREAM } from '../types';
+import { SET_SCREAMS, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_DATA, DELETE_SCREAM } from '../types';
 
 const initialState = {
   screams: [],
@@ -21,11 +21,21 @@ export default function(state = initialState, action) {
       }
     case LIKE_SCREAM:
     case UNLIKE_SCREAM:
-      let index = state.screams.findIndex((scream) => scream.screamId === action.payload.screamId);
+      var index = state.screams.findIndex(
+        (scream) => scream.screamId === action.payload.screamId
+      );
       state.screams[index] = action.payload;
       return {
         ...state
       };
+      case DELETE_SCREAM:
+        index = state.screams.findIndex(
+          (scream) => scream.screamId === action.payload
+        );
+        state.screams.splice(index, 1);
+        return {
+          ...state
+        };
     default: 
       return state;
   }
